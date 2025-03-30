dd01f7f88f4b9ae63f37821cc2905f677e6dcd0d
LapadJamaica <jamaicalapad70@gmail.com>
2025-03-30 10:18:14 +0800

    Add user creation functionality

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,6 +6,39 @@ import { newUser } from 'entities/User.interface';
 import { User } from '../entities/User'
 const router: express.Router = express.Router();
 
+//POST request
+router.post("/", createSchema, async (req : Request, res : Response, next : NextFunction) => {
+  const user : newUser = req.body;
+  if(await database.User.findOne({where: {username: user.username}})){
+      res.json({message: `Username "${user.username}" is already taken`});
+      return;
+  }
+
+  const newUser = database.User.create(user);
+
+  await newUser.save();
+
+  res.json({message: "User added successfully!"})
+})
+
+//PUT request
+router.put("/:id", updateSchema, async  (req : Request, res : Response, next : NextFunction) =>{
+  const { id } = req.params;
+  const updatedUser = req.body;
+  const user = await database.User.findOneBy({id: +id});
+  if(!user){
+     res.json({message: "User not found!"});
+     return;
+  }
+  if( updatedUser.username !== user.username && await database.User.findOne({where: {username: updatedUser.username}})){
+      res.json({message: `Username "${updatedUser.username}" is already taken`});
+      return;
+  }
+  Object.assign(user,updatedUser);
+  await user.save();
+  res.json({message: "User updated successfully!"})
+})
+
 function createSchema(req: Request, res: Response, next: NextFunction) {
     const schema = Joi.object({
         firstname: Joi.string().required(),
